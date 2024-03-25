ed73ceb4425ce85c882365788591d1294982674b
Lebedeva Anastasiya <programmer.lebedeva@gmail.com>
2024-03-25 10:17:07 +0300

    homework-21 Добавление ini файла для хранения данных

--- connect.php
+++ connect.php
@@ -4,12 +4,14 @@ session_start();
 
 function getDbConnection(): PDO
 {
-    $host = 'localhost';
-    $port = 5432;
-    $dbname = 'project';
+    $configData = parse_ini_file('config/config.ini');
+
+    $host = $configData['host'];
+    $port = $configData['port'];
+    $dbname = $configData['dbname'];
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-    $username = 'postgres';
-    $passwd = 'postgres';
+    $username = $configData['username'];
+    $passwd = $configData['passwd'];
     $pdo = new PDO($dsn, $username, $passwd);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
